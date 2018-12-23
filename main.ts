0eecab0728690a2f91e81db39c42b54c802e9d12
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2018-12-24 06:27:28 +0900

    add bittest

--- main.ts
+++ main.ts
@@ -20,4 +20,16 @@ namespace hex2number {
         }
         return r
     }
+    /**
+     * TODO: 数値の指定位置簿ビットの01を返す
+     * @param n 数値。, eg: 0x0010
+     * @param b ビット位置。, eg: 2
+     */
+    //% block
+    export function bittest(n: number,b:number): boolean {
+        if((n & 1<< b) != 0)
+            return true
+        else
+            return false
+    }
 }
